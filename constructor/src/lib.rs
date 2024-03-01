85b8e933b81f31aa3574252c1f3e429fb3a7f07e
epimore <kz986542@gmail.com>
2024-03-01 14:30:30 +0800

    添加readme

--- constructor/src/lib.rs
+++ constructor/src/lib.rs
@@ -23,8 +23,8 @@ fn drive(input: TokenStream, ident: &str) -> TokenStream {
     let ast: DeriveInput = syn::parse(input).expect("syn parse failed");
     let res = match ast.data {
         Data::Struct(ref s) => { handle_struct(&ast, &s.fields, ident) }
-        Data::Enum(_) => { panic!("doesn't work with enums yet") }
-        Data::Union(_) => { panic!("doesn't work with unions yet") }
+        Data::Enum(_) => { panic!("enum is not supported") }
+        Data::Union(_) => { panic!("union is not supported") }
     };
     res.into()
 }
@@ -42,11 +42,11 @@ fn handle_struct(ast: &DeriveInput, fields: &Fields, ident: &str) -> proc_macro2
             match ident {
                 IDENT_GET => {
                     let constructor_ident = ConstructorIdent::Get(true);
-                    build_constructor(ast, &fields.named, constructor_ident, ident)
+                    build_constructor(ast, &fields.named, constructor_ident)
                 }
                 IDENT_SET => {
                     let constructor_ident = ConstructorIdent::Set(true);
-                    build_constructor(ast, &fields.named, constructor_ident, ident)
+                    build_constructor(ast, &fields.named, constructor_ident)
                 }
                 &_ => { panic!("invalid ident") }
             }
@@ -55,51 +55,74 @@ fn handle_struct(ast: &DeriveInput, fields: &Fields, ident: &str) -> proc_macro2
             match ident {
                 IDENT_GET => {
                     let constructor_ident = ConstructorIdent::Get(false);
-                    build_constructor(ast, &fields.unnamed, constructor_ident, ident)
+                    build_constructor(ast, &fields.unnamed, constructor_ident)
                 }
                 IDENT_SET => {
                     let constructor_ident = ConstructorIdent::Set(false);
-                    build_constructor(ast, &fields.unnamed, constructor_ident, ident)
+                    build_constructor(ast, &fields.unnamed, constructor_ident)
                 }
                 &_ => { panic!("invalid ident") }
             }
         }
-        Fields::Unit => { panic!("Unit structs are not supported") }
+        Fields::Unit => { panic!("Unit struct is not supported") }
     }
 }
 
-fn build_constructor(ast: &DeriveInput, fields: &Punctuated<Field, Token![,]>, constructor_ident: ConstructorIdent, ident: &str) -> proc_macro2::TokenStream {
+fn build_named_fn<F>(f: F, ast: &DeriveInput, fields: &Punctuated<Field, Token![,]>, ident: &str) -> proc_macro2::TokenStream
+    where F: Fn(&Ident, &Type, Ident) -> proc_macro2::TokenStream {
     let args = parse_args::<Ident>(&ast.attrs, ident);
     let mut constructors = quote!();
-    for (index, field) in fields.iter().enumerate() {
+    for field in fields {
+        let field_type = &field.ty;
         let field_name = field.ident.as_ref().unwrap();
         if !contains_fields(&args, field_name) {
             continue;
         }
-        let field_type = &field.ty;
-        let constructor = match constructor_ident {
-            ConstructorIdent::Get(true) => {
-                let constructor_name = format_ident!("{ident}_{}",field_name);
-                build_named_get_constructor(field_name, field_type, constructor_name)
-            }
-            ConstructorIdent::Get(false) => {
-                let constructor_name = format_ident!("{ident}_{}",index);
-                build_unnamed_get_constructor(&Index::from(index), field_type, constructor_name)
-            }
-            ConstructorIdent::Set(true) => {
-                let constructor_name = format_ident!("{ident}_{}",field_name);
-                build_named_set_constructor(field_name, field_type, constructor_name)
-            }
-            ConstructorIdent::Set(false) => {
-                let constructor_name = format_ident!("{ident}_{}",index);
-                build_unnamed_set_constructor(&Index::from(index), field_type, constructor_name)
-            }
-        };
+        let constructor_name = format_ident!("{ident}_{}",field_name);
+        let constructor = f(field_name, field_type, constructor_name);
         constructors = quote! {
             #constructors
             #constructor
         };
     }
+    constructors
+}
+
+fn build_unnamed_fn<F>(f: F, ast: &DeriveInput, fields: &Punctuated<Field, Token![,]>, ident: &str) -> proc_macro2::TokenStream
+    where F: Fn(&Index, &Type, Ident) -> proc_macro2::TokenStream {
+    let args = parse_args::<Index>(&ast.attrs, ident);
+    let mut constructors = quote!();
+    for (index, field) in fields.iter().enumerate() {
+        let field_type = &field.ty;
+        let index = Index::from(index);
+        if !contains_fields(&args, &index) {
+            continue;
+        }
+        let constructor_name = format_ident!("{ident}_{}",index);
+        let constructor = f(&index, field_type, constructor_name);
+        constructors = quote! {
+            #constructors
+            #constructor
+        };
+    }
+    constructors
+}
+
+fn build_constructor(ast: &DeriveInput, fields: &Punctuated<Field, Token![,]>, constructor_ident: ConstructorIdent) -> proc_macro2::TokenStream {
+    let constructors = match constructor_ident {
+        ConstructorIdent::Get(true) => {
+            build_named_fn(build_named_get_constructor, ast, fields, IDENT_GET)
+        }
+        ConstructorIdent::Get(false) => {
+            build_unnamed_fn(build_unnamed_get_constructor, ast, fields, IDENT_GET)
+        }
+        ConstructorIdent::Set(true) => {
+            build_named_fn(build_named_set_constructor, ast, fields, IDENT_SET)
+        }
+        ConstructorIdent::Set(false) => {
+            build_unnamed_fn(build_unnamed_set_constructor, ast, fields, IDENT_SET)
+        }
+    };
     let name = &ast.ident;
     let (i, t, w) = ast.generics.split_for_impl();
     quote! {
@@ -165,5 +188,7 @@ fn parse_args<T: Parse>(attrs: &Vec<Attribute>, ident: &str) -> Option<Punctuate
 //未指定字段：全部字段为真
 //指定字段时：指定字段为真
 fn contains_fields<T: Parse + PartialEq>(args: &Option<Punctuated<T, Comma>>, item: &T) -> bool {
-    args.is_none() || args.as_ref().unwrap().iter().any(|arg| arg == item)
+    args.as_ref()
+        .map(|arg_list| arg_list.iter().any(|arg| arg == item))
+        .unwrap_or(true)
 }
